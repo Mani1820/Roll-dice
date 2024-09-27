d7a61bc2a31c120650aacf3e723a6fedbfd4b346
Mani <manibadboy66@gmail.com>
2024-09-27 11:51:30 +0530

    #

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:rolldice/style_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomCenter;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -6,8 +10,18 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors:)),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: const [
+            Colors.blue,
+            Colors.purple,
+            Colors.purpleAccent,
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(child: StyledText(),),
     );
   }
 }
---
+++ lib/style_text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'priya',
+      style: TextStyle(
+          fontSize: 29,
+          color: Colors.black),
+    );
+  }
+}
